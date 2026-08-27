8c01064b9485919648d0e026ded8821126d4d87d
hangtiancheng.htc <hangtiancheng.htc@taobao.com>
2026-08-27 13:16:05 +0800

    feat: Introduce new features

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,88 +4,9 @@ import {
   installMermaidFence,
   MERMAID_TAG,
 } from "@lark.js/docs";
-import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
-import type { PluginOption } from "vite";
 import { defineConfig } from "vitepress";
 import { excludePrivatePages, privateDocsPlugin } from "@swifty.js/docs";
-
-const plugins: PluginOption[] = [
-  tailwindcss(),
-  VitePWA({
-    registerType: "autoUpdate",
-    injectRegister: "script",
-    includeAssets: [
-      "favicon.svg",
-      "favicon.ico",
-      "apple-touch-icon-180x180.png",
-    ],
-    manifest: {
-      id: "/h/",
-      name: "homepage",
-      short_name: "homepage",
-      description: "homepage",
-      theme_color: "#f05138",
-      background_color: "#f05138",
-      display: "standalone",
-      scope: "/h/",
-      start_url: "/h/",
-      icons: [
-        {
-          src: "/h/pwa-64x64.png",
-          sizes: "64x64",
-          type: "image/png",
-        },
-        {
-          src: "/h/pwa-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "/h/pwa-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-        {
-          src: "/h/maskable-icon-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "maskable",
-        },
-      ],
-    },
-    workbox: {
-      globPatterns: ["**/*.{js,css,html,svg,png,woff2}"],
-      runtimeCaching: [
-        {
-          urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-          handler: "CacheFirst",
-          options: {
-            cacheName: "google-fonts-cache",
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 365,
-            },
-            cacheableResponse: { statuses: [0, 200] },
-          },
-        },
-        {
-          urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-          handler: "CacheFirst",
-          options: {
-            cacheName: "gstatic-fonts-cache",
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 365,
-            },
-            cacheableResponse: { statuses: [0, 200] },
-          },
-        },
-      ],
-    },
-  }),
-  privateDocsPlugin(),
-];
 
 export default defineConfig({
   srcDir: "docs",
@@ -94,9 +15,81 @@ export default defineConfig({
   description: "Swifty Homepage",
   base: "/h/",
   vite: {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    plugins,
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+        injectRegister: "script",
+        includeAssets: [
+          "favicon.svg",
+          "favicon.ico",
+          "apple-touch-icon-180x180.png",
+        ],
+        manifest: {
+          id: "/h/",
+          name: "homepage",
+          short_name: "homepage",
+          description: "homepage",
+          theme_color: "#f05138",
+          background_color: "#f05138",
+          display: "standalone",
+          scope: "/h/",
+          start_url: "/h/",
+          icons: [
+            {
+              src: "/h/pwa-64x64.png",
+              sizes: "64x64",
+              type: "image/png",
+            },
+            {
+              src: "/h/pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/h/pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "/h/maskable-icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+        },
+        workbox: {
+          globPatterns: ["**/*.{js,css,html,svg,png,woff2}"],
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+              handler: "CacheFirst",
+              options: {
+                cacheName: "google-fonts-cache",
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 365,
+                },
+                cacheableResponse: { statuses: [0, 200] },
+              },
+            },
+            {
+              urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+              handler: "CacheFirst",
+              options: {
+                cacheName: "gstatic-fonts-cache",
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 365,
+                },
+                cacheableResponse: { statuses: [0, 200] },
+              },
+            },
+          ],
+        },
+      }),
+      privateDocsPlugin(),
+    ],
     optimizeDeps: {
       exclude: ["@swifty.js/anti-copy", "@lark.js/docs"],
       // Prebundle the excluded package's nested dep so its CJS deps (dayjs) get ESM interop in dev.
