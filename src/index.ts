fc5b9fac77dbe4f1a13bdd402e97c78423fe80c8
hangtiancheng.htc <hangtiancheng.htc@taobao.com>
2026-09-08 13:47:59 +0800

    feat: Introduce new features

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,37 @@
-function shortestBeautifulSubstring(s: string, k: number): string {
-  const loc: number[] = [];
-
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === "1") {
-      loc.push(i);
-    }
-  }
-
-  const n = loc.length;
-  if (n < k) {
-    return "";
-  }
-  let len = s.length;
+function lexGreaterPermutation(s: string, target: string): string {
+  const used = Array.from({ length: s.length }, () => false);
   let ans = "";
-  for (let i = 0; i + k <= n; i++) {
-    const l = loc[i];
-    const r = loc[i + k - 1];
-    const len2 = r - l + 1;
-    const cur = s.slice(l, r + 1);
-    if (len === len2) {
-      ans = ans === "" ? cur : cur < ans ? cur : ans;
-      continue;
-    }
-    if (len2 < len) {
-      len = len2;
-      ans = cur;
-    }
-  }
 
+  const prefixCompare = (a: string, b: string) => {
+    const len = Math.min(a.length, b.length);
+    return a.slice(0, len).localeCompare(b.slice(0, len));
+  };
+  const dfs = (cur: string) => {
+    if (ans !== "") {
+      if (prefixCompare(cur, ans) > 0) {
+        return;
+      }
+    }
+
+    if (cur.length === target.length) {
+      if (cur.localeCompare(target) > 0) {
+        if (ans === "" || cur.localeCompare(ans) < 0) {
+          ans = cur;
+        }
+      }
+      return;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+      if (used[i]) {
+        continue;
+      }
+      used[i] = true;
+      dfs(cur + s[i]);
+      used[i] = false;
+    }
+  };
+
+  dfs("");
   return ans;
 }
