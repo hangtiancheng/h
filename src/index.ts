3d63a9464a785aec5081502e691b834effc5ad51
hangtiancheng.htc <hangtiancheng.htc@taobao.com>
2026-09-10 16:13:59 +0800

    feat: Introduce new features

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,66 @@
-function lexGreaterPermutation(s: string, target: string): string {
-  const used = Array.from({ length: s.length }, () => false);
-  let ans = "";
+function minMoves(classroom: string[], energy: number): number {
+  let target = 0;
 
-  const prefixCompare = (a: string, b: string) => {
-    const len = Math.min(a.length, b.length);
-    return a.slice(0, len).localeCompare(b.slice(0, len));
-  };
-  const dfs = (cur: string) => {
-    if (ans !== "") {
-      if (prefixCompare(cur, ans) > 0) {
-        return;
+  const grid = classroom.map((item) => {
+    const line = Array.from(item);
+    for (const item of line) {
+      if (item === "L") {
+        target++;
       }
     }
+    return line;
+  });
 
-    if (cur.length === target.length) {
-      if (cur.localeCompare(target) > 0) {
-        if (ans === "" || cur.localeCompare(ans) < 0) {
-          ans = cur;
-        }
-      }
+  let ans = Infinity;
+  const next = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+  ];
+
+  const dfs = (
+    x: number,
+    y: number,
+    count: number,
+    curSteps: number,
+    curEnergy: number,
+  ) => {
+    if (count === target) {
+      ans = Math.min(ans, curSteps);
       return;
     }
-
-    for (let i = 0; i < s.length; i++) {
-      if (used[i]) {
-        continue;
+    if (grid[x][y] !== "R" && curEnergy === 0) {
+      return;
+    }
+    if (grid[x][y] === "R") {
+      curEnergy = energy;
+    }
+    const xyL = grid[x][y] === "L";
+    for (const [dx, dy] of next) {
+      const x2 = x + dx;
+      const y2 = y + dy;
+      if (
+        x2 >= 0 &&
+        x2 < grid.length &&
+        y2 >= 0 &&
+        y2 < grid[0].length &&
+        grid[x2][y2] !== "X"
+      ) {
+        if (xyL) {
+          grid[x2][y2] = ".";
+        }
+        dfs(x2, y2, count + (xyL ? 1 : 0), curSteps + 1, curEnergy - 1);
+        if (xyL) {
+          grid[x2][y2] = "L";
+        }
       }
-      used[i] = true;
-      dfs(cur + s[i]);
-      used[i] = false;
     }
   };
 
-  dfs("");
-  return ans;
+  dfs(0, 0, 0, 0, energy);
+  return Number.isFinite(ans) ? ans : -1;
 }
+
+const ans = minMoves(["S.", "XL"], 2);
+console.log(ans);
